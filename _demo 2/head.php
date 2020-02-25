94f68b6103642cceeb796fe3ee7efbc24d2bb373
Ronny <1020ronny@gmail.com>
2020-02-25 18:50:13 +0800

    products demo

---
+++ _demo 2/head.php
@@ -0,0 +1,8 @@
+<div id="head">
+        <button class="openbtn btn" id='openbtn ' onclick="openNav()">☰ 展開選單</button>
+        <!-- <button class='outbtn' id='logout' onclick='logout()'>登出</button> -->
+        <form method='post'  class='logout_form' >
+            <button type='submit' class='outbtn btn' id='logout' name='logout'>登出</button>
+        </form>
+        <h3 style="float: right;">Hello, <?php print_r($_COOKIE['username']);?></h3>
+    </div>
--- _demo 2/products.php
+++ _demo 2/products.php
@@ -46,7 +46,50 @@ if (isset($_POST["logout"])) {
     <?php include 'head.php';?>
 
 <!-- Start your code here -->
-
+<div class ="main p-5">
+<form>
+<input type="submit" value="刪除" class="btn btn-danger mb-3">
+</form>
+  <table class="table table-striped ">
+    <thead class="thead-light">
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+        <th>Email</th>
+        <th>Email</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+        <td>john@example.com</td>
+        <td>john@example.com</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+        <td>john@example.com</td>
+        <td>john@example.com</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+        <td>john@example.com</td>
+        <td>john@example.com</td>
+        <td>john@example.com</td>
+      </tr>
+    </tbody>
+  </table>
+ 
+</div>
 
 <?php include 'footer.php';?>
 
