02f6152dfa2f197dc53235d36e07798cc79681a1
Ronny <1020ronny@gmail.com>
2020-02-26 16:47:39 +0800

    products select page

--- _demo/works/products.php
+++ _demo/works/products.php
@@ -16,6 +16,24 @@ if (isset($_POST["logout"])) {
     header('Location: ../index.php');
 }
 
+// get page
+if (isset($_GET["page"])) {
+    $page = $_GET["page"];
+} else {
+    $page = 1;
+}
+//number of rows
+
+if (isset($_POST["row_num_submit"])) {
+    $_SESSION["products_row_num"] = $_POST["row_num"];
+} else if (isset($_SESSION["products_row_num"])) {
+} else {
+    $_SESSION["products_row_num"] = 50;
+}
+$rowNum = $_SESSION["products_row_num"];
+
+
+
 //connect to SQL
 header("content-type:text/html; charset=utf-8");
 $link = @mysqli_connect("localhost", "root", "") or die(mysqli_connect_error());
@@ -52,6 +70,7 @@ if (isset($_POST["deleteSelected"])) {
     header('location:' . $_SERVER['REQUEST_URI'] . '');
 }
 
+
 ?>
 
 <!DOCTYPE html>
@@ -82,86 +101,95 @@ if (isset($_POST["deleteSelected"])) {
     <?php include '../parts/sidebar.php';?>
     <?php include '../parts/head.php';?>
 
-<!-- Start your code here -->
-<div class="main p-5">
+    <!-- Start your code here -->
+    <div class="main p-5">
 
-    <form method='post' class="card p-3">
-        <div>
-            <input type="submit" value="刪除勾選" name="deleteSelected" onclick="return confirm('你確定要刪除勾選資料嗎？')"
-                class="btn btn-danger mb-3">
-            <input type="submit" value="新增資料" class="btn btn-primary ml-3 mb-3">
+        <form method='post' class="card p-3">
+            <div>
+                <input type="submit" value="刪除勾選" name="deleteSelected" onclick="return confirm('你確定要刪除勾選資料嗎？')"
+                    class="btn btn-danger mb-3">
+                <input type="submit" value="新增資料" class="btn btn-primary ml-3 mb-3">
 
-            <div class='float-right'>
-                <label for="row_num_select">請選擇顯示行數:</label>
-                <select id="row_num_select" name="row_num">
-                    <option value="10">10</option>
-                    <option value="20">20</option>
-                    <option value="50" selected>50</option>
-                    <option value="100">100</option>
-                </select>
-                <input type="submit" value="確定" name="row_num_submit" class="btn btn-primary ml-3 mb-3">
+                <div class='float-right'>
+                    <label for="row_num_select">請選擇顯示行數:</label>
+                    <select id="row_num_select" name="row_num">
+                        <option value="10" <?php if ($rowNum == 10) {echo "selected";}?>>10 </option>
+                        <option value="20" <?php if ($rowNum == 20) {echo "selected";}?>>20 </option>
+                        <option value="50" <?php if ($rowNum == 50) {echo "selected";}?>>50 </option>
+                        <option value="100" <?php if ($rowNum == 100) {echo "selected";}?>>100</option>
+                    </select>
+                    <input type="submit" value="確定" name="row_num_submit" class="btn btn-primary ml-3 mb-3">
+                </div>
             </div>
+
+            <table class="table table-striped ">
+                <thead class="thead-light">
+                    <tr>
+                        <th>check</th>
+                        <th>productID</th>
+                        <th>ProductName</th>
+                        <th>sellerID</th>
+                        <th>UnitPrice</th>
+                        <th>UnitsInStock</th>
+                        <th></th>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                    <?php
+// write table
+$tableOffSet = $rowNum * $page;
+$commandText = <<<SqlQuery
+                    select productID, ProductName, sellerID, UnitPrice, UnitsInStock from coffee.products ORDER BY productID LIMIT $rowNum OFFSET $tableOffSet
+                    SqlQuery;
+$result = mysqli_query($link, $commandText);
+
+while ($row = mysqli_fetch_assoc($result)):
+
+?>
+
+                    <tr>
+                        <td>
+                            <input type="checkbox" name="<?php echo "selected" . $row["productID"] ?>"
+                                class="btn btn-danger mb-3">
+                        </td>
+                        <td><?php echo $row["productID"] ?></td>
+                        <td><?php echo $row["ProductName"] ?></td>
+                        <td><?php echo $row["sellerID"] ?></td>
+                        <td><?php echo $row["UnitPrice"] ?></td>
+                        <td><?php echo $row["UnitsInStock"] ?>
+                        <td class="p-0">
+                            <input type="submit" value="刪除" name="<?php echo "delete" . $row["productID"] ?>"
+                                class="btn btn-danger mb-3" onclick="return confirm('你確定要刪除這筆資料嗎？')">
+                            <input type="submit" value="編輯" class="btn btn-primary mb-3">
+                        </td>
+                    </tr>
+                    <?php endwhile?>
+                </tbody>
+
+            </table>
+        </form>
+
+
+        <div class="d-flex justify-content-center m-5">
+        <!-- page select -->
+        <?php
+
+        $total_num_rows = mysqli_num_rows(mysqli_query($link,"select productID from coffee.products;"));
+        $lastPage=floor($total_num_rows/$rowNum);
+        if($lastPage<9999999){
+            for($i=1 ; $i<=$lastPage ; $i++ ){
+                echo " <a class='m-1' href='products.php?page=$i'>$i</a>";
+            }
+        }
+        ?>
+    
         </div>
 
-        <table class="table table-striped ">
-            <thead class="thead-light">
-                <tr>
-                    <th>check</th>
-                    <th>productID</th>
-                    <th>ProductName</th>
-                    <th>sellerID</th>
-                    <th>UnitPrice</th>
-                    <th>UnitsInStock</th>
-                    <th></th>
-                </tr>
-            </thead>
+    </div>
 
-            <tbody>
-
-                <?php
-// write table
-$commandText = <<<SqlQuery
-        select productID, ProductName, sellerID, UnitPrice, UnitsInStock
-        from coffee.products
-        SqlQuery;
-
-//number of rows
-$result = mysqli_query($link, $commandText);
-if (isset($_POST["row_num_submit"])) {
-    $rowNum = $_POST["row_num"];
-}else{
-    $rowNum = 50;
-}
-$rowCount = 0;
-while (null !== ($row = mysqli_fetch_assoc($result)) && $rowCount < $rowNum):
-    $rowCount++;
-    ?>
-
-		                <tr>
-		                    <td>
-		                        <input type="checkbox" name="<?php echo "selected" . $row["productID"] ?>"
-		                            class="btn btn-danger mb-3">
-		                    </td>
-		                    <td><?php echo $row["productID"] ?></td>
-		                    <td><?php echo $row["ProductName"] ?></td>
-		                    <td><?php echo $row["sellerID"] ?></td>
-		                    <td><?php echo $row["UnitPrice"] ?></td>
-		                    <td><?php echo $row["UnitsInStock"] ?>
-		                    <td class="p-0">
-		                        <input type="submit" value="刪除" name="<?php echo "delete" . $row["productID"] ?>"
-		                            class="btn btn-danger mb-3" onclick="return confirm('你確定要刪除這筆資料嗎？')">
-		                        <input type="submit" value="編輯" class="btn btn-primary mb-3">
-		                    </td>
-		                </tr>
-		                <?php endwhile?>
-            </tbody>
-
-        </table>
-    </form>
-
-</div>
-
-<!-- End your code here. -->
-<?php include '../parts/footer.php';?>
+    <!-- End your code here. -->
+    <?php include '../parts/footer.php';?>
 
 </body>
