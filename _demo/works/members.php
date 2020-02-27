ac77c3f356ea4465f749d925618b6445fd937a31
ckhomo <ck9030311@gmail.com>
2020-02-27 19:22:00 +0800

    WRITE IN!

--- _demo/works/members.php
+++ _demo/works/members.php
@@ -10,7 +10,7 @@ if ($_SESSION['username'] == '' || $_SESSION['AorS'] != 0) {
 //Logout:
 //清空SESSION
 if (isset($_POST["logout"])) {
-
+    // session_destroy();
     $_SESSION['username'] = '';
     $_SESSION['AorS'] = '';
     header('Location: ../index.php');
@@ -22,6 +22,41 @@ $link = @mysqli_connect("localhost", "root", "") or die(mysqli_connect_error());
 $result = mysqli_query($link, "set names utf8");
 mysqli_select_db($link, "coffee");
 
+//========== ADD: ADJUST COLUMN. ==========//
+
+// get page
+if (isset($_GET["page"])) {
+    $page = $_GET["page"];
+} else {
+    $page = 1;
+}
+//number of rows
+if (isset($_POST["row_num_submit"])) {
+    $_SESSION["member_row_num"] = $_POST["row_num"];
+    header('Location: members.php');
+} else if (isset($_SESSION["member_row_num"])) {
+} else {
+//初始欄數=50
+    $_SESSION["member_row_num"] = 50;
+}
+$rowNum = $_SESSION["member_row_num"];
+
+//總欄數:
+$total_num_rows = mysqli_num_rows(mysqli_query($link, "select customerID from coffee.customers;"));
+//最後一頁的頁數為:
+$lastPage = floor($total_num_rows / $rowNum) + 1;
+$tableOffSet = $rowNum * ($page - 1);
+$showDataStartFrom = $tableOffSet + 1;
+$showDataEndTo = $tableOffSet + $rowNum;
+if ($showDataEndTo > $total_num_rows) {
+    $showDataEndTo = $total_num_rows;
+}
+;
+$previousPage = $page - 1;
+$nextPage = $page + 1;
+
+//===== ENDED HERE. =====//
+
 //2 Buttons work here.
 foreach ($_POST as $i => $j) {
     //Right delete button:
@@ -32,9 +67,8 @@ foreach ($_POST as $i => $j) {
         SqlQuery;
         mysqli_query($link, $deleteCommandText);
         header('location:' . $_SERVER['REQUEST_URI'] . '');
-
-        //Right edit button:
-    } elseif (substr($i, 0, 4) == "edit") {
+    } //Right edit button:
+    elseif (substr($i, 0, 4) == "edit") {
         //獲得customerID
         $editItem = ltrim($i, "edit");
         $editCommandText = <<<SqlQuery
@@ -56,12 +90,20 @@ foreach ($_POST as $i => $j) {
     }
 }
 
-// Output at modal.
-function wModal($mystr)
-{
-    echo ($mystr);
-    // setcookie($mystr, '', time() - 3600);
-    unset($mystr);
+//ADD NEW DATA TO FORM! :
+if (isset($_POST['modal_submit'])) {
+    $tmp_cid = $_POST['cid'];
+    $tmp_nam = $_POST['nam'];
+    $tmp_acc = $_POST['acc'];
+    $tmp_pwd = $_POST['pwd'];
+    $tmp_sex = $_POST['sex'];
+    $tmp_bid = $_POST['bid'];
+    $tmp_adr = $_POST['adr'];
+    $tmp_mob = $_POST['mob'];
+    $insertCommandText = <<<SqlQuery
+    insert into coffee.customers VALUES ('$tmp_cid','$tmp_nam','$tmp_acc','$tmp_pwd','$tmp_sex','$tmp_bid','$tmp_adr','$tmp_mob')
+    SqlQuery;
+    mysqli_query($link, $insertCommandText);
 }
 
 // Write table:
@@ -132,12 +174,26 @@ if (isset($_POST["deleteSelected"])) {
         <!--Modal toggled here.-->
         <input type="button" value="新增資料" name="edit" class="btn btn-primary ml-3 mb-3"
                data-toggle="modal" data-target="#myModal">
-    </div>
+                <div class='float-right'>
+                    <span class="mr-5">
+                    <!-- show where you are -->
+                    <?php echo "您在第 $page 頁，顯示資料為 $showDataStartFrom - $showDataEndTo 筆(共 $total_num_rows 筆資料)" ?>
+                    </span>
+                    <label for="row_num_select">請選擇顯示行數:</label>
+                    <select id="row_num_select" name="row_num">
+                        <option value="10" <?php if ($rowNum == 10) {echo "selected";}?>>10 </option>
+                        <option value="20" <?php if ($rowNum == 20) {echo "selected";}?>>20 </option>
+                        <option value="50" <?php if ($rowNum == 50) {echo "selected";}?>>50 </option>
+                        <option value="100" <?php if ($rowNum == 100) {echo "selected";}?>>100</option>
+                    </select>
+                    <input type="submit" value="確定" name="row_num_submit" class="btn btn-primary ml-3 mb-3">
+                    </span>
+                </div>
 
     <table class="table table-striped ">
         <thead class="thead-light">
             <tr>
-                <th>check</th>
+            <th><input type="checkbox" id="selectAll" onclick="selectAllCheckbox()"><label for="selectAll">全選</label></th>
                 <th>customerID</th>
                 <th>cName</th>
                 <th>cAccount</th>
@@ -153,9 +209,10 @@ if (isset($_POST["deleteSelected"])) {
 <?php
 // write table
 // $commandText: $str
+// 受所允許之總欄數調控
 $commandText = <<<SqlQuery
 select customerID, cName, cAccount, cSex, cBirthDate, cAddress, cMobile
-from coffee.customers
+from coffee.customers ORDER BY customerID LIMIT $rowNum OFFSET $tableOffSet
 SqlQuery;
 
 $result = mysqli_query($link, $commandText);
@@ -177,19 +234,56 @@ while ($row = mysqli_fetch_assoc($result)): ?>
                     <input type="submit" value="刪除" name="<?php echo "delete" . $row["customerID"] ?>"
                         class="btn btn-danger mb-3" onclick="return confirm('你確定要刪除這筆資料嗎？')">
                     <!--Modal aslo toggled at here.-->
-                    <input type='submit' value="編輯" name="<?php echo "edit" . $row["customerID"] ?>"
-                        class="btn btn-primary mb-3" data-toggle="modal" data-target="#myModal">
+                    <input type='button' value="編輯" name="<?php echo "edit" . $row["customerID"] ?>"
+                        class="btn btn-primary mb-3">
                 </td>
             </tr>
             <?php endwhile?>
         </tbody>
     </table>
 </form>
+<!--頁尾頁碼&按鈕顯示:-->
+<div class="d-flex justify-content-center align-items-center flex-column  m-5">
+        <!-- page select -->
+        <div class="m-3">
+            <a class='m-2 btn btn-info' href='members.php?page=1'>第一頁</a>
+            <a class='m-2 btn btn-info' href='members.php?page=<?php echo ($page <= 1) ? "1" : $previousPage; ?>'>上一頁</a>
+            <a class='m-2 btn btn-info' href='members.php?page=<?php echo ($page >= $lastPage) ? $lastPage : $nextPage; ?>'>下一頁</a>
+            <a class='m-2 btn btn-info' href='members.php?page=<?php echo $lastPage; ?>'>最尾頁</a>
+        </div>
+            <div>
+            <?php
+for ($i = 1; $i <= 3 && $i <= $lastPage; $i++) {
+    echo " <a class='m-2' href='members.php?page=$i'>$i</a>";
+}
+if ($page <= 6) {
+    for ($i = 4; $i <= ($page + 2) && $i <= $lastPage; $i++) {
+        echo " <a class='m-2' href='members.php?page=$i'>$i</a>";
+    }
+} else {
+    echo "<span>......</span>";
+    for ($i = ($page - 2); $i <= ($page + 2) && $i <= $lastPage; $i++) {
+        echo " <a class='m-2' href='members.php?page=$i'>$i</a>";
+    }
+}
+if ($lastPage - $page <= 5) {
+    for ($i = ($page + 3); $i <= $lastPage; $i++) {
+        echo " <a class='m-2' href='members.php?page=$i'>$i</a>";
+    }
+} else {
+    echo "<span>......</span>";
+    for ($i = ($lastPage - 2); $i <= $lastPage; $i++) {
+        echo " <a class='m-2' href='members.php?page=$i'>$i</a>";
+    }
+}
+
+?>
+<!--頁尾頁碼&按鈕結束-->
 </div>
 <!-- Dummy frame. -->
 <iframe name="thisframe"></iframe>
 <!-- Modal -->
-<div class="modal" id="myModal">
+<div class="modal fade" id="myModal">
 <div class="modal-dialog">
     <div class="modal-content">
 
@@ -198,37 +292,39 @@ while ($row = mysqli_fetch_assoc($result)): ?>
             <h4 class="modal-title">資料變更:</h4>
             <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
-
+        <form method="post" action=''>
         <!-- Modal body -->
         <div class="modal-body">
             <tr>
-                <form method="post">
-                    <th>customerID:<input type="text" name='cid ' value="<?php wModal($_SESSION["modal_cid"]);?>">
+
+                    <th>customerID:<input type="text" name='cid'>
                     </th>
                     <hr>
-                    <th>cName: <input type="text" name='nam' value="<?php wModal($_SESSION["modal_nam"]);?>"></th>
+                    <th>cName: <input type="text" name='nam'></th>
                     <hr>
-                    <th>cAccount: <input type="text" name='acc' value="<?php wModal($_SESSION["modal_acc"]);?>">
+                    <th>cAccount: <input type="text" name='acc'>
                     </th>
                     <hr>
-                    <th>cSex: <input type="text" name='sex' value="<?php wModal($_SESSION["modal_sex"]);?>"></th>
-                    <hr>
-                    <th>cBirthDate:<input type="text" name='bid' value="<?php wModal($_SESSION["modal_bid"]);?>">
+                    <th>cPassword: <input type="text" name='pwd'>
                     </th>
                     <hr>
-                    <th>cAddress: <input type="text" name='adr' value="<?php wModal($_SESSION["modal_adr"]);?>">
+                    <th>cSex: <input type="text" name='sex'></th>
+                    <hr>
+                    <th>cBirthDate:<input type="date" name='bid'>
                     </th>
                     <hr>
-                    <th>cMobile: <input type="text" name='mob' value="<?php wModal($_SESSION["modal_mob"]);?>"></th>
-                </form>
+                    <th>cAddress: <input type="text" name='adr'>
+                    </th>
+                    <hr>
+                    <th>cMobile: <input type="text" name='mob'></th>
             </tr>
         </div>
-
         <!-- Modal footer -->
         <div class="modal-footer">
-            <button type="button" class="btn btn-primary" data-dismiss="modal">Submit</button>
+            <input type="submit" name="modal_submit" value='submit' class="btn btn-primary"></input>
             <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
         </div>
+        </form>
     </div>
 </div>
 </div>
