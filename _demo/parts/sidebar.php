1f720e1754292c9157b84d58179e88dd1db2187e
ckhomo <ck9030311@gmail.com>
2020-02-27 13:39:18 +0800

    2020.02.27

--- _demo/parts/sidebar.php
+++ _demo/parts/sidebar.php
@@ -11,11 +11,11 @@ $error = "";
 
 <?php if ($_SESSION['AorS'] === 0) { ?>
     <a href="members.php">會員管理</a>   
-    <a href="#">廠商管理</a> 
+    <a href="sellers.php">廠商管理</a> 
 <?php } elseif ($_SESSION['AorS'] === 1) {  ?>
     <a href="#">訂單管理</a>
     <a href="products.php">商品管理</a>
     <a href="#">資料管理</a> 
-    <a href="#">行銷管理</a>
+    <a href="discount.php">行銷管理</a>
 <?php  } ?>
 </div>
--- _demo/works/members.php
+++ _demo/works/members.php
@@ -22,8 +22,9 @@ $link = @mysqli_connect("localhost", "root", "") or die(mysqli_connect_error());
 $result = mysqli_query($link, "set names utf8");
 mysqli_select_db($link, "coffee");
 
-// right delete btn
+//2 Buttons work here.
 foreach ($_POST as $i => $j) {
+    //Right delete button:
     if (substr($i, 0, 6) == "delete") {
         $deleteItem = ltrim($i, "delete");
         $deleteCommandText = <<<SqlQuery
@@ -31,10 +32,51 @@ foreach ($_POST as $i => $j) {
         SqlQuery;
         mysqli_query($link, $deleteCommandText);
         header('location:' . $_SERVER['REQUEST_URI'] . '');
+
+        //Right edit button:
+    } elseif (substr($i, 0, 4) == "edit") {
+        //獲得customerID
+        $editItem = ltrim($i, "edit");
+        $editCommandText = <<<SqlQuery
+        select customerID, cName, cAccount, cSex, cBirthDate, cAddress, cMobile
+        from coffee.customers WHERE customerID IN ('$editItem')
+        SqlQuery;
+        $result = mysqli_query($link, $editCommandText);
+        while ($row = mysqli_fetch_assoc($result)) {
+            $_SESSION["modal_cid"] = $row["customerID"];
+            $_SESSION["modal_nam"] = $row["cName"];
+            $_SESSION["modal_acc"] = $row["cAccount"];
+            $_SESSION["modal_sex"] = $row["cSex"];
+            $_SESSION["modal_bid"] = $row["cBirthDate"];
+            $_SESSION["modal_adr"] = $row["cAddress"];
+            $_SESSION["modal_mob"] = $row["cMobile"];
+        }
+        ;
+        // header('location:' . $_SERVER['REQUEST_URI'] . '');
     }
 }
 
-// delete selected items
+// Output at modal.
+function wModal($mystr)
+{
+    echo ($mystr);
+    // setcookie($mystr, '', time() - 3600);
+    unset($mystr);
+}
+
+// Write table:
+$front_STR1 = "<td>";
+$back_STR1 = "</td>";
+$front_STR2 = "<td><input type='textbox' value='";
+$back_STR2 = "'></td>";
+$res_fSTR = $front_STR1;
+$res_bSTR = $back_STR1;
+
+// $editbtn = "<input type='submit' value='編輯' class='btn btn-primary mb-3' name='to_edit'>";
+// $updtbtn = "<input type='submit' value='送出' class='btn btn-primary mb-3' name='to_updt'>";
+// $resultBtn = $editbtn;
+
+// delete selected items:
 if (isset($_POST["deleteSelected"])) {
     $selectedList = "!";
 
@@ -46,8 +88,8 @@ if (isset($_POST["deleteSelected"])) {
     }
     $selectedList = ltrim($selectedList, "!,");
     $deleteSelectedCommandText = <<<SqlQuery
-  DELETE FROM coffee.customers WHERE customerID IN ($selectedList)
-  SqlQuery;
+    DELETE FROM coffee.customers WHERE customerID IN ($selectedList)
+    SqlQuery;
     mysqli_query($link, $deleteSelectedCommandText);
     header('location:' . $_SERVER['REQUEST_URI'] . '');
 }
@@ -83,66 +125,113 @@ if (isset($_POST["deleteSelected"])) {
 <!-- Start your code here. -->
 <div class="main p-5">
 
-    <form method='post' class="card p-3">
-        <div>
-            <input type="submit" value="刪除勾選" name="deleteSelected" onclick="return confirm('你確定要刪除勾選資料嗎？')"
-                class="btn btn-danger mb-3">
-            <input type="submit" value="新增資料" class="btn btn-primary ml-3 mb-3">
+<form method='post' class="card p-3">
+    <div>
+        <input type="submit" value="刪除勾選" name="deleteSelected" onclick="return confirm('你確定要刪除勾選資料嗎？')"
+            class="btn btn-danger mb-3">
+        <!--Modal toggled here.-->
+        <input type="button" value="新增資料" name="edit" class="btn btn-primary ml-3 mb-3"
+               data-toggle="modal" data-target="#myModal">
+    </div>
+
+    <table class="table table-striped ">
+        <thead class="thead-light">
+            <tr>
+                <th>check</th>
+                <th>customerID</th>
+                <th>cName</th>
+                <th>cAccount</th>
+                <th>cSex</th>
+                <th>cBirthDate</th>
+                <th>cAddress</th>
+                <th>cMobile</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+
+<?php
+// write table
+// $commandText: $str
+$commandText = <<<SqlQuery
+select customerID, cName, cAccount, cSex, cBirthDate, cAddress, cMobile
+from coffee.customers
+SqlQuery;
+
+$result = mysqli_query($link, $commandText);
+while ($row = mysqli_fetch_assoc($result)): ?>
+
+            <tr>
+                <td>
+                    <input type="checkbox" name="<?php echo "selected" . $row["customerID"] ?>" class='checkmark'
+                        style='position: relative;'>
+                </td>
+                <?php echo $res_fSTR . $row["customerID"] . $res_bSTR ?>
+                <?php echo $res_fSTR . $row["cName"] . $res_bSTR ?>
+                <?php echo $res_fSTR . $row["cAccount"] . $res_bSTR ?>
+                <?php echo $res_fSTR . $row["cSex"] . $res_bSTR ?>
+                <?php echo $res_fSTR . $row["cBirthDate"] . $res_bSTR ?>
+                <?php echo $res_fSTR . $row["cAddress"] . $res_bSTR ?>
+                <?php echo $res_fSTR . $row["cMobile"] . $res_bSTR ?>
+                <td>
+                    <input type="submit" value="刪除" name="<?php echo "delete" . $row["customerID"] ?>"
+                        class="btn btn-danger mb-3" onclick="return confirm('你確定要刪除這筆資料嗎？')">
+                    <!--Modal aslo toggled at here.-->
+                    <input type='submit' value="編輯" name="<?php echo "edit" . $row["customerID"] ?>"
+                        class="btn btn-primary mb-3" data-toggle="modal" data-target="#myModal">
+                </td>
+            </tr>
+            <?php endwhile?>
+        </tbody>
+    </table>
+</form>
+</div>
+<!-- Dummy frame. -->
+<iframe name="thisframe"></iframe>
+<!-- Modal -->
+<div class="modal" id="myModal">
+<div class="modal-dialog">
+    <div class="modal-content">
+
+        <!-- Modal Header -->
+        <div class="modal-header">
+            <h4 class="modal-title">資料變更:</h4>
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
 
-        <table class="table table-striped ">
-            <thead class="thead-light">
-                <tr>
-                    <th>check</th>
-                    <th>customerID</th>
-                    <th>cName</th>
-                    <th>cAccount</th>
-                    <th>cSex</th>
-                    <th>cBirthDate</th>
-                    <th>cAddress</th>
-                    <th>cMobile</th>
-                    <th></th>
-                </tr>
-            </thead>
+        <!-- Modal body -->
+        <div class="modal-body">
+            <tr>
+                <form method="post">
+                    <th>customerID:<input type="text" name='cid ' value="<?php wModal($_SESSION["modal_cid"]);?>">
+                    </th>
+                    <hr>
+                    <th>cName: <input type="text" name='nam' value="<?php wModal($_SESSION["modal_nam"]);?>"></th>
+                    <hr>
+                    <th>cAccount: <input type="text" name='acc' value="<?php wModal($_SESSION["modal_acc"]);?>">
+                    </th>
+                    <hr>
+                    <th>cSex: <input type="text" name='sex' value="<?php wModal($_SESSION["modal_sex"]);?>"></th>
+                    <hr>
+                    <th>cBirthDate:<input type="text" name='bid' value="<?php wModal($_SESSION["modal_bid"]);?>">
+                    </th>
+                    <hr>
+                    <th>cAddress: <input type="text" name='adr' value="<?php wModal($_SESSION["modal_adr"]);?>">
+                    </th>
+                    <hr>
+                    <th>cMobile: <input type="text" name='mob' value="<?php wModal($_SESSION["modal_mob"]);?>"></th>
+                </form>
+            </tr>
+        </div>
 
-            <tbody>
-
-                <?php
-    // write table
-    $commandText = <<<SqlQuery
-        select customerID, cName, cAccount, cSex, cBirthDate, cAddress, cMobile
-        from coffee.customers
-        SqlQuery;
-    
-    $result = mysqli_query($link, $commandText);
-    while ($row = mysqli_fetch_assoc($result)): ?>
-
-                <tr>
-                    <!-- <label class="form-check-label"> -->
-                        <td>
-                            <input type="checkbox" name="<?php echo "selected" . $row["customerID"] ?>"
-                                class="btn btn-danger mb-3">
-                        </td>
-                        <td><?php echo $row["customerID"] ?></td>
-                        <td><?php echo $row["cName"] ?></td>
-                        <td><?php echo $row["cAccount"] ?></td>
-                        <td><?php echo $row["cSex"] ?></td>
-                        <td><?php echo $row["cBirthDate"] ?></td>
-                        <td><?php echo $row["cAddress"] ?></td>
-                        <td><?php echo $row["cMobile"] ?></td>
-                        <td>
-                            <input type="submit" value="刪除" name="<?php echo "delete" . $row["customerID"] ?>"
-                                class="btn btn-danger mb-3" onclick="return confirm('你確定要刪除這筆資料嗎？')">
-                            <input type="submit" value="編輯" class="btn btn-primary mb-3">
-                </tr>
-                <?php endwhile?>
-            </tbody>
-
-        </table>
-    </form>
-
+        <!-- Modal footer -->
+        <div class="modal-footer">
+            <button type="button" class="btn btn-primary" data-dismiss="modal">Submit</button>
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+    </div>
+</div>
 </div>
 <!-- End your code here. -->
 <?php include '../parts/footer.php';?>
-
 </body>
