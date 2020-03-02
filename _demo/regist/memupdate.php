d88a527db44c32dcb632176f640c8b7ddc7980cc
Linpoche <59193156+Linpoche@users.noreply.github.com>
2020-03-02 10:09:21 +0800

    修正bug

--- _demo/regist/memupdate.php
+++ _demo/regist/memupdate.php
@@ -1,61 +1,135 @@
 <?php
+function GetSQLValueString($theValue, $theType) {
+  switch ($theType) {
+    case "string":
+      $theValue = ($theValue != "") ? filter_var($theValue, FILTER_SANITIZE_MAGIC_QUOTES) : "";
+      break;
+    case "int":
+      $theValue = ($theValue != "") ? filter_var($theValue, FILTER_SANITIZE_NUMBER_INT) : "";
+      break;
+    case "email":
+      $theValue = ($theValue != "") ? filter_var($theValue, FILTER_VALIDATE_EMAIL) : "";
+      break;
+    case "url":
+      $theValue = ($theValue != "") ? filter_var($theValue, FILTER_VALIDATE_URL) : "";
+      break;      
+  }
+  return $theValue;
+}
+require_once("connMysql.php");
 session_start();
-include("connMysql.php");
-error_reporting(0);
+
 $username=$_SESSION['username'];
 $findID=mysqli_query($db_link,"select sellerID from coffee.sellers WHERE sAccount='$username';");
-$findNA=mysqli_query($db_link,"select sName from coffee.sellers WHERE sAccount='$username';");
-$findPW=mysqli_query($db_link,"select sPassword from coffee.sellers WHERE sAccount='$username';");
-$findPH=mysqli_query($db_link,"select sPhone from coffee.sellers WHERE sAccount='$username';");
-$findAD=mysqli_query($db_link,"select sAddress from coffee.sellers WHERE sAccount='$username';");
-$findMA=mysqli_query($db_link,"select sMail from coffee.sellers WHERE sAccount='$username';");
-$findCT=mysqli_query($db_link,"select sCountry from coffee.sellers WHERE sAccount='$username';");
-
 $ID = mysqli_fetch_assoc($findID);
-$NA = mysqli_fetch_assoc($findNA);
-$PW = mysqli_fetch_assoc($findPW);
-$PH = mysqli_fetch_assoc($findPH);
-$AD = mysqli_fetch_assoc($findAD);
-$MA = mysqli_fetch_assoc($findMA);
-$CT = mysqli_fetch_assoc($findCT);
-
 $userID = $ID["sellerID"];
-$name = $NA["sName"];
-$password = $PW["sPassword"];
-$phone = $PH["sPhone"];
-$address = $AD["sAddress"];
-$mail = $MA["sMail"];
-$country = $CT["sCountry"];
-    
 
+//檢查是否經過登入
+if(!isset($_SESSION['username']) || ($_SESSION['username']=="")){
+	header("Location: ../index.php");
+}
+//執行登出動作
+if(isset($_GET["logout"]) && ($_GET["logout"]=="true")){
+	unset($_SESSION["username"]);
+	
+	header("Location: ../index.php");
+}
+//重新導向頁面
+$redirectUrl="../_main.php";
+//執行更新動作
+if(isset($_POST["action"])&&($_POST["action"]=="update")){	
+	$query_update = "UPDATE sellers SET sPassword=?, sName=?, sMail=?, sCountry=?, sPhone=?, sAddress=? WHERE sellerID=?";
+	$stmt = $db_link->prepare($query_update);
+	//檢查是否有修改密碼
+	$mpass = $_POST["sPasswordo"];
+	if(($_POST["sPassword"]!="")&&($_POST["sPassword"]==$_POST["sPasswordrecheck"])){
+		$mpass = password_hash($_POST["sPassword"], PASSWORD_DEFAULT);
+	}
+	$stmt->bind_param("ssssssi", 
+		$mpass,
+		GetSQLValueString($_POST["sName"], 'string'),
+	
+		GetSQLValueString($_POST["sMail"], 'email'),
+		GetSQLValueString($_POST["sCountry"], 'string'),
+		GetSQLValueString($_POST["sPhone"], 'string'),
+		GetSQLValueString($_POST["sAddress"], 'string'),		
+		GetSQLValueString($_POST["sellerID"], 'int'));
+	$stmt->execute();
+	$stmt->close();
+	//若有修改密碼，則登出回到首頁。
+	if(($_POST["sPassword"]!="")&&($_POST["sPassword"]==$_POST["sPasswordrecheck"])){
+		unset($_SESSION["username"]);
+		
+		$redirectUrl="../index.php";
+	}		
+	//重新導向
+	header("Location: $redirectUrl");
+}
 
-
-
-$MyName=$_POST["sName"];
-$MyPhone=$_POST["sPhone"];
-$MyPassword=$_POST["sPassword"];
-$MyAddress=$_POST["sAddress"];
-$MyMail=$_POST["sMail"];
-$MyCountry=$_POST["sCountry"];
-$sql_query = "UPDATE sellers SET sName='$MyName', sPassword='$MyPassword', sPhone='$MyPhone', sAddress='$MyAddress', sMail='$MyMail', sCountry='$MyCountry' WHERE sellerID='$userID' ";   
-$stmt = $db_link -> prepare($sql_query);
-$stmt -> execute();
-$stmt -> close();
-$db_link -> close();
-
-
-
+//繫結登入會員資料
+$query_RecMember = "SELECT * FROM sellers WHERE sellerID='$userID'";
+$RecMember = $db_link->query($query_RecMember);	
+$row_RecMember = $RecMember->fetch_assoc();
 ?>
-
-
-
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>註冊資訊修改系統</title>
+<title>網站會員系統</title>
 <link href="style.css" rel="stylesheet" type="text/css">
 <script language="javascript">
-
+function checkForm(){
+	if(document.formJoin.sPassword.value!="" || document.formJoin.sPasswordrecheck.value!=""){
+		if(!check_passwd(document.formJoin.sPassword.value,document.formJoin.sPasswordrecheck.value)){
+			document.formJoin.sPassword.focus();
+			return false;
+		}
+	}	
+	if(document.formJoin.sName.value==""){
+		alert("請填寫姓名!");
+		document.formJoin.sName.focus();
+		return false;
+	}
+	
+	if(document.formJoin.sMail.value==""){
+		alert("請填寫電子郵件!");
+		document.formJoin.sMail.focus();
+		return false;
+	}
+	if(!checkmail(document.formJoin.sMail)){
+		document.formJoin.sMail.focus();
+		return false;
+	}
+	return confirm('確定送出嗎？');
+}
+function check_passwd(pw1,pw2){
+	if(pw1==''){
+		alert("密碼不可以空白!");
+		return false;
+	}
+	for(var idx=0;idx<pw1.length;idx++){
+		if(pw1.charAt(idx) == ' ' || pw1.charAt(idx) == '\"'){
+			alert("密碼不可以含有空白或雙引號 !\n");
+			return false;
+		}
+		if(pw1.length<5 || pw1.length>10){
+			alert( "密碼長度只能5到10個字母 !\n" );
+			return false;
+		}
+		if(pw1!= pw2){
+			alert("密碼二次輸入不一樣,請重新輸入 !\n");
+			return false;
+		}
+	}
+	return true;
+}
+function checkmail(sMail) {
+	var filter  = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	if(filter.test(sMail.value)){
+		return true;
+	}
+	alert("電子郵件格式不正確");
+	return false;
+}
 </script>
 </head>
 
@@ -68,41 +142,41 @@ $db_link -> close();
     <td class="tdbline"><table width="100%" border="0" cellspacing="0" cellpadding="10">
       <tr valign="top">
         <td class="tdrline"><form action="" method="POST" name="formJoin" id="formJoin" onSubmit="return checkForm();">
-          <p class="title">修改註冊資料</p>
+          <p class="title">修改資料</p>
           <div class="dataDiv">
             <hr size="1" />
             <p class="heading">帳號資料</p>
-            <p><strong>使用帳號</strong>：<?php echo $userID;?></p>
+            <p><strong>使用帳號</strong>：<?php echo $row_RecMember["sAccount"];?></p>
             <p><strong>使用密碼</strong>：
             <input name="sPassword" type="password" class="normalinput" id="sPassword">
-    		<input name="sPasswordo" type="hidden" id="sPasswordo" value="<?php echo $password;?>"></p>
+    		<input name="sPasswordo" type="hidden" id="sPasswordo" value="<?php echo $row_RecMember["sPassword"];?>"></p>
             <p><strong>確認密碼</strong> ：
             <input name="sPasswordrecheck" type="password" class="normalinput" id="sPasswordrecheck"><br>
             <span class="smalltext">若不修改密碼，請不要填寫。若要修改，請輸入密碼</span><span class="smalltext">二次。<br>若修改密碼，系統會自動登出，請用新密碼登入。</span></p>
             <hr size="1" />
-            <p class="heading">廠商資料</p>
-            <p><strong>公司名稱</strong>：
-            <input name="sName" type="text" class="normalinput" id="sName" value="<?php echo $name;?>">
+            <p class="heading">個人資料</p>
+            <p><strong>真實姓名</strong>：
+            <input name="sName" type="text" class="normalinput" id="sName" value="<?php echo $row_RecMember["sName"];?>">
             <font color="#FF0000">*</font></p>
+            
+            <p><strong>電子郵件</strong>：
+            <input name="sMail" type="text" class="normalinput" id="sMail" value="<?php echo $row_RecMember["sMail"];?>">
+            <font color="#FF0000">*</font><br><span class="smalltext">請確定此電子郵件為可使用狀態，以方便未來系統使用，如補寄會員密碼信。</span></p>
             <p><strong>國　　家</strong>：
-            <input name="sCountry" type="text" class="normalinput" id="sCountry" value="<?php echo $country;?>">
-            <font color="#FF0000">*</font></p>
-           
+            <input name="sCountry" type="text" class="normalinput" id="sCountry" value="<?php echo $row_RecMember["sCountry"];?>">
+            <br><span class="smalltext"></span></p>
             <p><strong>電　　話</strong>：
-            <input name="sPhone" type="text" class="normalinput" id="sPhone" value="<?php echo $phone;?>">
-            <font color="#FF0000">*</font></p>
-            <p><strong>信　　箱</strong>：
-            <input name="sMail" type="text" class="normalinput" id="sMail" value="<?php echo $mail;?>" size="40"></p>
-            <p><strong>地　　址</strong>：
-            <input name="sAddress" type="text" class="normalinput" id="sAddress" value="<?php echo $address;?>" size="40">
-            <font color="#FF0000">*</font></p>
+            <input name="sPhone" type="text" class="normalinput" id="sPhone" value="<?php echo $row_RecMember["sPhone"];?>"></p>
+            <p><strong>住　　址</strong>：
+            <input name="sAddress" type="text" class="normalinput" id="sAddress" value="<?php echo $row_RecMember["sAddress"];?>" size="40"> </p>
             <p><font color="#FF0000">*</font> 表示為必填的欄位</p>
           </div>
           <hr size="1" />
           <p align="center">
-            <input name="m_id" type="hidden" id="m_id" value="<?php echo $row_RecMember["m_id"];?>">
+            <input name="sellerID" type="hidden" id="sellerID" value="<?php echo $row_RecMember["sellerID"];?>">
             <input name="action" type="hidden" id="action" value="update">
             <input type="submit" name="Submit2" value="修改資料">
+            <input type="reset" name="Submit3" value="重設資料">
             <input type="button" name="Submit" value="回上一頁" onClick="window.history.back();">
           </p>
         </form></td>
@@ -111,11 +185,11 @@ $db_link -> close();
         <div class="regbox">
           <p class="heading"><strong>會員系統</strong></p>
           
-            <p><strong><?php echo $name;?></strong> 您好。</p>
+            <p><strong><?php echo $row_RecMember["sName"];?></strong> 您好。</p>
             
-            本次登入的日期為：<br>
+            本次登入的時間為：<br>
             <?php echo date("Y年m月d日") ;?></p>
-            <p align="center"><a href="../works/_main.php">回到主頁</a> | <a href="../index.php">登出系統</a></p>
+            <p align="center"><a href="../works/_main.php">會員中心</a> | <a href="?logout=true">登出系統</a></p>
         </div>
         <div class="boxbl"></div><div class="boxbr"></div></td>
       </tr>
@@ -127,3 +201,6 @@ $db_link -> close();
 </table>
 </body>
 </html>
+<?php
+	$db_link->close();
+?>
