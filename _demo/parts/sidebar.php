9aad63c3c1a0b540bf101f82b68eea6475b35664
Linpoche <59193156+Linpoche@users.noreply.github.com>
2020-03-03 18:46:57 +0800

    更改

--- _demo/parts/sidebar.php
+++ _demo/parts/sidebar.php
@@ -6,18 +6,21 @@ $error = "";
 
 <div id="mySidebar" class="sidebar">
     <div class="sidebar_bg"></div>
+
     <div class="sidebar_context">
-    <img src="../image/sidebar-head.jpg" alt="sidebar-head" class="sidebar-head" width="250px">
-        <form method='post'  class='logout_form' >
-        <button type='submit' class='outbtn btn' id='logout' name='logout'>登出</button>
-        </form>
+        <img src="../image/sidebar-head.jpg" alt="sidebar-head" class="sidebar-head" width="250px">
+
         <p href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</p>
     <!-- Add welcome message here. -->
         <p class="hello">Hello, <?php print_r($_SESSION['username']);?></p>
-        <br><br><hr>
+        <form method='post'  class='logout_form' >
+        <button type='submit' class='outbtn btn' id='logout' name='logout'>登出</button>
+        </form>
+        <hr>
         <?php if ($_SESSION['AorS'] === 0) {?>
             <!-- Collapsible bar here. -->
-            <a class='triangle_title' type="button" data-toggle="collapse" data-target="#member">
+            <a class='triangle_title' type="button" data-toggle="collapse" data-target="#member"
+               onclick="rotateTarget('triangle_rotate')">
                 會員管理<div class="triangle_rotate">▶</div></a>
             <div id="member" class="collapse">
             <a href="members.php">會員資料</a>
@@ -26,11 +29,24 @@ $error = "";
             <a class='triangle_title' href="sellers.php">廠商管理<div class="triangle_rotate">▶</div></a>
         <?php } elseif ($_SESSION['AorS'] === 1) {?>
             
-            <a href="../regist/memupdate.php">資料管理</a>
-            <a href="orders.php">訂單管理</a>
-            <a href="products.php">商品管理</a>
-            <a href="infomations.php">訊息管理</a>        
-            <a href="discounts.php">優惠管理</a>
+            <a class='triangle_title' type="button" data-toggle="collapse" data-target="#member"
+               onclick="rotateTarget('triangle_rotate')">
+               資料管理<div class="triangle_rotate">▶</div></a>
+            <div id="member" class="collapse">
+            <a href="../regist/memupdate.php">註冊管理</a>
+            <a href="../works/infomations.php">訊息管理</a>
+            <a href="../works/discounts.php">優惠管理</a>
+            </div>
+
+            
+            <a href="../works/orders.php">訂單管理</a>
+            <a href="../works/products.php">商品管理</a>
+            
+            
+            
 <?php }?>
     </div>
+
+
+
 </div>
